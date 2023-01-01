f3ebe4ddd7aaf08e5abf5bd863707c6de6793160
Sergei <sergej.klykow@gmail.com>
2023-01-02 00:20:59 +0100

    Start implementing conversion hints for pol-s

---
+++ docs/index_page.js
@@ -0,0 +1,4 @@
+// the way of adding the Event listener - to guarantee that the page and all elements are available
+document.addEventListener("DOMContentLoaded", function (){
+    console.log("Page loaded");
+})
