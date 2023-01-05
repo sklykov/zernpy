96eb506d2431bd1be42c1e356b9ecefc12df99e9
Sergei <sergej.klykow@gmail.com>
2023-01-05 18:03:57 +0100

    Updated API Dictionary

--- build_api_dict_pdoc.bat
+++ build_api_dict_pdoc.bat
@@ -1,2 +1,2 @@
-pdoc ./src/zernpy/zernikepol.py --no-show-source -o ./docs/api --logo ../for_favicon.png --footer-text "zernpy ver. 0.0.4, 2022 Sergei Klykov"
+pdoc ./src/zernpy/zernikepol.py --no-show-source -o ./docs/api --logo ../for_favicon.png --footer-text "zernpy ver. 0.0.5, 2022 Sergei Klykov"
 timeout /t 30
