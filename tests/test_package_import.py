62ea7e639f224d071e73972a9b2bd28166ad3d5d
Sergei <sergej.klykow@gmail.com>
2025-02-12 16:46:27 +0100

    Added more tests for checking generated installation package

--- tests/test_package_import.py
+++ tests/test_package_import.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Test the imports of "zernpy" package.
+Test the import of "zernpy" package for basic functionality.
 
-@author: Sergei Klykov, @year: 2024, @licence: MIT
+@author: Sergei Klykov, @year: 2025, @licence: MIT
 
 """
 
 
 def test_initialization():
     try:
-        from zernpy import ZernPol, generate_polynomials, ZernPSF
+        from zernpy import ZernPol, generate_polynomials, ZernPSF, force_get_psf_compilation
+        force_get_psf_compilation(True)
         zp = ZernPol(m=-2, n=2)
         assert len(zp.get_polynomial_name()) > 0, "Failed simple function call for getting polynomial name"
         pols = generate_polynomials(4)
         assert "Piston" in str(pols[0]), "Generated polynomials not started with Piston"
-        zpsf = ZernPSF(zp); zpsf.set_physical_props(NA=1.25, wavelength=0.405, expansion_coeff=0.5, pixel_physical_size=0.405/5.85)
+        zpsf = ZernPSF(ZernPol(m=-1, n=1))
+        zpsf.set_physical_props(NA=0.85, wavelength=0.4, expansion_coeff=0.01, pixel_physical_size=0.4/4.9)
+        zpsf.set_calculation_props(kernel_size=17, n_integration_points_r=100, n_integration_points_phi=180)
+        zpsf.calculate_psf_kernel(normalized=True, verbose_info=True); zpsf.crop_kernel()
     except ImportError:
         import os
         os.chdir("..")  # navigate to the root folder of the project
@@ -22,8 +26,8 @@ def test_initialization():
         zp = zernikepol.ZernPol(m=-2, n=2)  # initialize Zernike ppolynomial ZernPol class
         zpsf = zernpsf.ZernPSF(zp)  # initialize ZernPSF class
         zpsf.set_physical_props(NA=1.25, wavelength=0.405, expansion_coeff=0.5, pixel_physical_size=0.405/5.85)
-        print("zernpy package not installed, this test automatically passed")
+        print("zernpy package not installed in the current Python environment, this test automatically passed")
 
 
 if __name__ == "__main__":
-    test_initialization()
+    test_initialization()  # run it for checking after installation of generated Python package
