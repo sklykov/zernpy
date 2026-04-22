d261ec79597aecb1a304df339ee8abda8c94b6c8
Sergei <sergej.klykow@gmail.com>
2026-04-22 18:14:55 +0200

    Added autoimport of 'clean_zernpy_cache', cleaned up build of dist instructions

--- src/zernpy/__init__.py
+++ src/zernpy/__init__.py
@@ -12,7 +12,7 @@ __version__ = "0.1.1"  # Straightforward way of specifying package version and i
 
 # Univesal logic for making all main classes and functions available after calling 'from project import *'
 from .zernikepol import ZernPol, fit_polynomials, fit_polynomials_vectors, generate_phases_image, generate_polynomials, generate_random_phases
-from .zernpsf import ZernPSF, force_get_psf_compilation
+from .zernpsf import ZernPSF, clean_zernpy_cache, force_get_psf_compilation
 
 __all__ = ["ZernPol", "generate_polynomials", "fit_polynomials", "generate_random_phases", "fit_polynomials_vectors",
-           "generate_phases_image", "ZernPSF", "force_get_psf_compilation"]
+           "generate_phases_image", "ZernPSF", "force_get_psf_compilation", "clean_zernpy_cache"]
