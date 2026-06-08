Possible fix scaling issue for PSF calculation of Zernike polynomials

src/zernpy/tests/test_calc_psf.py
@@ -40,9 +40,9 @@ def test_psf_kernel_calc():
     assert np.max(diff_airy) < 0.01, ("Difference between exact Airy pattern and calculated by the numerical integral is bigger than 1%:"
                                       + str(np.max(diff_airy)))
     # Test normal calculation of several polynomials
-    pols = (ZernPol(osa=10), ZernPol(osa=15)); coeffs = (0.08, -0.07)
+    pols = (ZernPol(osa=10), ZernPol(osa=15)); coeffs = (0.06, -0.05)
     zpsf = ZernPSF(pols); zpsf.set_physical_props(NA, wavelength, expansion_coeff=coeffs, pixel_physical_size=wavelength / 2.8)
-    zpsf.set_calculation_props(kernel_size=23, n_integration_points_r=150, n_integration_points_phi=120)
+    zpsf.set_calculation_props(kernel_size=25, n_integration_points_r=150, n_integration_points_phi=120)
     psf_kernel = zpsf.calculate_psf_kernel(); psf_kernel_size = zpsf.kernel_size
     w_orig_kernel, h_orig_kernel = psf_kernel.shape
     assert np.max(psf_kernel) > 0.5 and np.min(psf_kernel) > -1E-5, "Check calculation of a kernel for several polynomials"
@@ -50,10 +50,10 @@ def test_psf_kernel_calc():
     zpsf.crop_kernel()  # cropping the calculated kernel
     cropped_kernel = zpsf.kernel; cropped_kernel_size = zpsf.kernel_size
     w_crop_kernel, h_crop_kernel = cropped_kernel.shape
-    assert psf_kernel_size > cropped_kernel_size, (f"Cropped kernel size ({psf_kernel_size}) is equal "
+    assert psf_kernel_size >= cropped_kernel_size, (f"Cropped kernel size ({psf_kernel_size}) is equal "
                                                    + f"or more than original ({cropped_kernel_size})")
-    assert w_orig_kernel > w_crop_kernel and h_orig_kernel > h_crop_kernel, (f"Cropped kernel shape ({cropped_kernel.shape}) is equal "
-                                                                             + f"or more than original({psf_kernel.shape})")
+    assert w_orig_kernel >= w_crop_kernel and h_orig_kernel >= h_crop_kernel, (f"Cropped kernel shape ({cropped_kernel.shape}) is equal "
+                                                                              + f"or more than original({psf_kernel.shape})")
 
 
 def test_zernpsf_usage():

src/zernpy/zernpsf.py
@@ -160,7 +160,8 @@ class ZernPSF:
         expansion_coeff : float | Sequence[float]
             Amplitude(-s) or expansion coefficient(-s) of the Zernike polynomial in physical units.
             Note that according to the used equation for PSF calculation it will be adjusted to the units of wavelength:
-            alpha = expansion_coeff/wavelength. See the equation in the method "calculate_psf_kernel". \n
+            alpha = 2.0*pi*expansion_coeff/wavelength. See the equation in the method "calculate_psf_kernel". \n
+            !!!: alpha definition changed (multiplied by 2*pi) after version 0.1.0 \n
             Note that if Airy pattern (PSF for Piston polynomial) is provided, it's required to provide amplitude for it.
             However, this amplitude will be ignored in general for calculation because of its properties.
         pixel_physical_size : float
@@ -208,10 +209,10 @@ class ZernPSF:
                 if coeffs_len == 1 and len(self.polynomials) == 0:  # polynomials maybe provided also as a sequence with 1 element
                     expansion_coeff = float(expansion_coeff[0])
                     # Sanity check for the expansion coefficient of the polynomial
-                    self.__warn_message = _sanity_check_expansion_coefficient(abs(expansion_coeff) / wavelength)
+                    self.__warn_message = _sanity_check_expansion_coefficient(abs(2.0*pi*expansion_coeff) / wavelength)
                     if len(self.__warn_message) > 0:
                         warnings.warn(self.__warn_message, stacklevel=2); self.__warn_message = ""
-                    self.expansion_coeff = expansion_coeff; self.alpha = self.expansion_coeff / self.wavelength
+                    self.expansion_coeff = expansion_coeff; self.alpha = (2.0*pi*self.expansion_coeff) / self.wavelength
                 else:
                     raise ValueError(f"Length of provided coefficients ({coeffs_len}) is not equal to stored number "
                                      + f"of polynomials ({len(self.polynomials)})")
@@ -219,10 +220,10 @@ class ZernPSF:
                 self.coefficients = np.asarray(expansion_coeff)  # conversion to efficient array format
                 # Sanity check for the maximum expansion coefficient of the polynomial
                 max_module_coeff = max(np.max(self.coefficients), abs(np.min(self.coefficients)))
-                self.__warn_message = _sanity_check_expansion_coefficient(abs(max_module_coeff) / wavelength, max_coeff_check=True)
+                self.__warn_message = _sanity_check_expansion_coefficient(abs(2.0*pi*max_module_coeff) / wavelength, max_coeff_check=True)
                 if len(self.__warn_message) > 0:
                     warnings.warn(self.__warn_message, stacklevel=2); self.__warn_message = ""
-                self.amplitudes = self.coefficients / self.wavelength
+                self.amplitudes = (2.0*pi*self.coefficients) / self.wavelength
         else:
             # Check consistency of provided type of polynomials and coefficients
             if self.zernpol is None and len(self.polynomials) > 1:  # only if 2 and more polynomials provided
@@ -232,10 +233,10 @@ class ZernPSF:
             if not isinstance(expansion_coeff, float):
                 expansion_coeff = float(expansion_coeff)
             # Sanity check for the expansion coefficient of the polynomial
-            self.__warn_message = _sanity_check_expansion_coefficient(abs(expansion_coeff) / wavelength)
+            self.__warn_message = _sanity_check_expansion_coefficient(abs(2.0*pi*expansion_coeff) / wavelength)
             if len(self.__warn_message) > 0:
                 warnings.warn(self.__warn_message, stacklevel=2); self.__warn_message = ""
-            self.expansion_coeff = expansion_coeff; self.alpha = self.expansion_coeff / self.wavelength
+            self.expansion_coeff = expansion_coeff; self.alpha = (2.0*pi*self.expansion_coeff) / self.wavelength
         # Kernel size estimation (could be changed explicitly in the method 'set_calculation_props'). Redefine it for each call
         if self.zernpol is not None:  # for single polynomial
             self.kernel_size = get_kernel_size(zernike_pol=self.zernpol, len2pixels=self.pixel_size, alpha=self.alpha,